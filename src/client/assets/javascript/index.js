27f8c3008dbd25022dc2d1b50f03e96c31ccf05b
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-02-27 16:49:18 +0000

    fixed issue with raceProgress positions and playerId- accelerate post request not yet working

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -365,7 +365,7 @@ function resultsView(positions) {
 }
 
 function raceProgress(positions) {
-  let userPlayer = positions.find((e) => e.id === store.player_id);
+  let userPlayer = positions.find((e) => e.id === parseInt(store.player_id));
   userPlayer.driver_name += " (you)";
 
   positions = positions.sort((a, b) => (a.segment > b.segment ? -1 : 1));
